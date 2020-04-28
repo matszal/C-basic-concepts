49b3438f2dfa9111d19ce2224339cd6acfc8dd3c
Mateusz Szalkowski <mszalkowski2011@gmail.com>
2020-04-28 17:56:17 +0100

    initial commit 2

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+        }
+    }
+}
